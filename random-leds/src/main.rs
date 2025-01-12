c267a2a74fc5d7220e0c7a12d307908034fbe4fd
Ray Suliteanu <raysuliteanu@gmail.com>
2025-01-11 17:07:05 -0800

    refactor to rust workspace
    prepare for multiple examples by refactoring the project into a
    workspace

---
+++ random-leds/src/main.rs
@@ -0,0 +1,58 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use microbit::board::Board;
+use microbit::display::blocking::Display;
+use microbit::hal::timer::Timer;
+use microbit::pac::TIMER0;
+use panic_halt as _;
+use rand::prelude::*;
+
+const DELAY: u32 = 100;
+
+struct LedMatrix {
+    timer: Timer<TIMER0>,
+    display: Display,
+    matrix: [[u8; 5]; 5],
+}
+
+impl LedMatrix {
+    fn new(board: Board) -> Self {
+        let timer = Timer::new(board.TIMER0);
+        let display = Display::new(board.display_pins);
+        LedMatrix {
+            timer,
+            display,
+            matrix: [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+            ],
+        }
+    }
+
+    fn show(&mut self, row: usize, col: usize) {
+        self.matrix[row][col] = 1;
+        self.display.show(&mut self.timer, self.matrix, DELAY);
+        self.matrix[row][col] = 0;
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let board = Board::take().unwrap();
+    let mut led_matrix = LedMatrix::new(board);
+
+    let mut rng = SmallRng::seed_from_u64(0xdeadbeef);
+
+    // infinite loop; just so we don't leave this stack frame
+    loop {
+        let r: usize = rng.gen_range(0..5);
+        let c: usize = rng.gen_range(0..5);
+
+        led_matrix.show(r, c);
+    }
+}
